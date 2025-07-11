39ec40bb3ae58441a86578ae42466fe9a385d45b
Aditya Sharma <adisharma4857@gmail.com>
2025-07-11 17:08:22 +0530

    add: shadcn-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-7xl font-bold">Your AI Powered Job Assistant</h1>
+      <h1 className="text-6xl font-bold">Your AI Powered Job Assistant</h1>
       {/* Coming Soon */}
-      <h2 className="text-3xl font-medium mt-10">Coming Soon... Stay tuned ✌️</h2>
+      <h2 className="text-3xl font-medium my-10">Coming Soon... Stay tuned ✌️</h2>
+      <Button className="cursor-pointer text-2xl p-6">Test Me 😊</Button>
     </main>
   );
 }
